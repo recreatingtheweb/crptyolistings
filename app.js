5c3e93feff461d6c35cc34b1d7fd40942903de50
recreatingtheweb <recreatingtheweb@gmail.com>
2022-09-02 21:08:59 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,66 @@
+var apiKey = "coinrankingef782bb78d6e6f94f8bbcfd8d6cfd581c5e3cdad46dbc2f7";
+var proxyUrl = "http://localhost:3002/coins";
+const coinList = document.getElementById("data");
+const filterInput = document.getElementById("filter");
+
+$(document).ready(function(){
+  if(window.matchMedia("(max-width: 454px)").matches){
+      // The viewport is less than 768 pixels wide
+      alert("Sorry there is no mobile version available. This website is best suited for larger displays such as Tablet or Desktop.");
+  } 
+});
+
+
+
+
+
+  let coinsData = []
+let filteredCoins = []
+
+var formatCash = n => {
+  if (n < 1e3) return n;
+  if (n >= 1e4) return + (n / 1e9).toFixed(2) + "billion ETC"
+}
+
+filterInput.addEventListener('keyup', (e) => {
+    // const inputValue = e.target.value
+  
+    filteredCoins = coinsData.filter(coin => {
+      return coin.name.toLowerCase().includes(e.target.value.toLowerCase())
+    })
+    displayCoins(filteredCoins)
+  })
+
+
+
+ const loadCoins = async () => {
+    try {
+      const res = await fetch(proxyUrl)
+      const dataResponse = await res.json()
+      coinsData = dataResponse.data.coins
+      console.log(dataResponse)
+      displayCoins(dataResponse.data.coins)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  const displayCoins = (coins) => {
+    const htmlString = coins.map((coin) => {
+      return `
+      <tr>
+        <td>${coin.name}</td>
+        <td>${coin.rank}</td>
+        <td>${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(coin.price)}</td>
+        <td>${formatCash(coin.marketCap)}</td>
+        <td>${coin.symbol}</td>
+        <td><img src="${coin.iconUrl}" height="25" width="25" /></td>
+       
+      </tr>
+      `
+    })
+      .join('');
+    coinList.innerHTML = htmlString
+     }
+  loadCoins()
---
+++ server.js
@@ -0,0 +1,56 @@
+// const express = require('express');
+// const cors = require('cors')
+// const morgan = require('morgan')
+// const fetch = require('node-fetch')
+
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import fetch from 'node-fetch';
+
+
+const app = express()
+app.use(cors())
+app.use(morgan("coins"))
+const port = 3002
+
+
+var apiKey = "coinrankingef782bb78d6e6f94f8bbcfd8d6cfd581c5e3cdad46dbc2f7";
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if ('OPTIONS' == req.method) {
+     res.sendStatus(200);
+   }
+   else {
+     next();
+   }});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/coins", (req, res) => {
+  const url = "https://api.coinranking.com/v2/coins";
+  (async () => {
+    try {
+      await fetch(`${url}`, {
+        headers: { "x-access-token": `${apiKey}` }
+      }).then((response) => response.json())
+        .then((json) => {
+          console.log(json)
+          res.json(json)
+        })
+    } catch (error) {
+      console.log(error)
+    }
+  })()
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
